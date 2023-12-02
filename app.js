ae1f7601f38453c6549a569ad6e5d3072615c2c4
Hamza <qomaise3@gmail.com>
2023-12-03 02:23:33 +0500

    VueJsHowSimple

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const inputEl=document.querySelector('input');
-const btn=document.querySelector('button');
-const listEl=document.querySelector('ul');
-function Goal(){
-const inputVal=inputEl.value;
-const listEle=document.createElement('li');
-listEle.textContent=inputVal;
-listEl.appendChild(listEle);
-inputEl.value=''
+Vue.createApp({
+data(){
+return{
+goals:[],
+enteredValue:''
 }
-btn.addEventListener('click',Goal)
+},
+methods:{
+addGoals(){
+this.goals.push(this.enteredValue);
+this.enteredValue=''
+}
+}
+}).mount('#app')
